b0863d3b70fd764899092ef9730a0faad573070a
Jordan Duran <jordanduranm@gmail.com>
2019-09-05 20:47:21 -0400

    getting json movie data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const MOVIES = require('./MOVIES.json')
@@ -10,12 +11,50 @@ app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
 
-app.use((req, res) => {
+app.get('/movies',(req, res) => {
   res.send(MOVIES);
 })
 
-const PORT = 8000
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.use(function validateToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    
+    if (authToken !== apiToken) {
+        return res
+            .status(401)
+            .send(`Unauthorized Access`)
+    }
+    next()
 })
+
+app.use(validateToken);
+
+app.get('/movies', (req, res) => {
+    let result = MOVIES;
+    
+    if (req.query.genre) {
+        result = result.filter(movie =>
+          movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+      }
+    
+      if (req.query.country) {
+        result = result.filter(movie =>
+          movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+      }
+    
+      if (req.query.avg_vote) {
+        result = result.filter(movie =>
+          Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        )
+      }
+    
+      res.json(result)
+    })
+    
+    const PORT = 8000
+    
+    app.listen(PORT, () => {
+      console.log(`Server listening at http://localhost:${PORT}`)
+    })
