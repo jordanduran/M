265f3e5a13715a652475991de845e4fc645aacae
Jordan Duran <jordanduranm@gmail.com>
2019-09-07 11:28:00 -0400

    error handling

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const helmet = require('helmet')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function(req, res, next) {
     const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
+    const authToken = req.get('Authorization').split('')[1]
     
     if (authToken !== apiToken) {
         return res
@@ -46,9 +47,19 @@ app.get('/movies', (req, res) => {
     
       res.json(result)
     })
+
+    app.use((error, req, res, next) => {
+      let response
+      if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }}
+      } else {
+        response = { error }
+      }
+      res.status(500).json(response)
+    })
     
-    const PORT = 8000
-    
+    const PORT = process.env.PORT || 8000
+
     app.listen(PORT, () => {
       console.log(`Server listening at http://localhost:${PORT}`)
     })
